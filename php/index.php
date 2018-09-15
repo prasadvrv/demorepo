e63b2274bd112eeb144e6d14e012c858cb1c3425
prasadvrv <prasadvrv@gmail.com>
2018-09-15 12:32:53 +0530

    Modified on 15th Sep

--- php/index.php
+++ php/index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World test on 13th Jun at 09:30 IST</p>'; ?> 
+ <?php echo '<p>Hello World test on 15th Sep at 12:30 IST</p>'; ?> 
  </body>
 </html>
